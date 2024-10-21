5ef5cbe987f30a0f6911050e42315861781ae4da
JulioFavaro98 <juliofv66@gmail.com>
2024-10-21 20:21:44 -0300

    Criando teste para UserService metodo findAll

--- src/test/java/br/com/julio/api/services/UserServiceTest.java
+++ src/test/java/br/com/julio/api/services/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,10 +23,11 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class UserServiceTest {
 
-    private Integer ID      = 1;
-    private String NAME     = "Julio";
-    private String EMAIL    = "julio@gmail.com";
-    private String SENHA    = "123";
+    private static final Integer ID                      = 1;
+    private static final String NAME                     = "Julio";
+    private static final String EMAIL                    = "julio@gmail.com";
+    private static final String SENHA                    = "123";
+    private static final String OBJETO_NAO_ENCONTRADO    = "Objeto não encontrado";
 
     @InjectMocks
     private UserService service;
@@ -60,18 +62,28 @@ class UserServiceTest {
 
     @Test
     void quandoFindByIdEntaoRetorneObjectNotFoundException() {
-        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
 
         try{
             repository.findById(ID);
         } catch (Exception ex) {
             assertEquals(ObjectNotFoundException.class, ex.getClass());
-            assertEquals("Objeto não encontrado", ex.getMessage());
+            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
         }
     }
 
     @Test
-    void findAll() {
+    void quandoFindAllEntaoRetorneUmaListaDeUser() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(0).getClass());
+        assertEquals(ID, response.get(0).getId());
+        assertEquals(NAME, response.get(0).getName());
+        assertEquals(EMAIL, response.get(0).getEmail());
     }
 
     @Test
