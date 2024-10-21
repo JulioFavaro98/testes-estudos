3376575fee8b9d400f61ca7e422199a803dbe1b7
JulioFavaro98 <juliofv66@gmail.com>
2024-10-21 16:39:37 -0300

    Criando teste para UserService metodo findById

---
+++ src/test/java/br/com/julio/api/services/UserServiceTest.java
@@ -0,0 +1,81 @@
+package br.com.julio.api.services;
+
+import br.com.julio.api.domain.User;
+import br.com.julio.api.domain.dto.UserDTO;
+import br.com.julio.api.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceTest {
+
+    private Integer ID      = 1;
+    private String NAME     = "Julio";
+    private String EMAIL    = "julio@gmail.com";
+    private String SENHA    = "123";
+
+    @InjectMocks
+    private UserService service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> userOptional;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void quandoFindByIdEntaoRetorneInstanciaUser() {
+        when(repository.findById(anyInt())).thenReturn(userOptional);
+        User response = service.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, SENHA);
+        userDTO = new UserDTO(ID, NAME, EMAIL, SENHA);
+        userOptional = Optional.of(new User(ID, NAME, EMAIL, SENHA));
+    }
+}
